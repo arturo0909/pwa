d39ebc82fd0149a8c335a871c50bc4ffdf584413
Arturo Acosta <arturo.acosta@cecar.edu.co>
2024-08-07 17:09:19 -0500

    proyecto inicializado a repositorio

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from 'cors';
+import db from "./db.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }));
+app.get("/", async(req, res) => {
+  try {
+    const sql = "SELECT * FROM writing";
+    const [result] = await db.query(sql);
+    res.json(result);
+    console.log(result);
+    console.table(result);
+  } catch (error) {
+    console.error("Database error:", error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server escuchando en el puerto 3000");
+});
